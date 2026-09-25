b1118686849832659acb8684950159e839049c85
kojiro5150 <42211188+kojiro5150@users.noreply.github.com>
2026-09-25 21:01:33 +1000

    chore: advance instruction pin to recovered Reality contract

--- src/runtime/instruction-loader.js
+++ src/runtime/instruction-loader.js
@@ -1,4 +1,4 @@
-export const INSTRUCTION_COMMIT = "56ad2305ca62ed7409c3e89723f9bd1ca914d935";
+export const INSTRUCTION_COMMIT = "e79f79378a9eba6f36c8d053a896ca00b3827c7b";
 export const GITHUB_BASE = "https://cdn.jsdelivr.net/gh/kojiro5150/phdss-instructions@"+INSTRUCTION_COMMIT+"/";
 
 export const INSTRUCTION_FILES = Object.freeze({
